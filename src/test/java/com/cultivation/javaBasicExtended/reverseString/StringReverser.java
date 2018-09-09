6ff944a7713a72bab19203463c5bcc2662c5a090
ycao <ycao@thoughtworks.com>
2018-09-09 17:28:49 +0800

    complete stringReverseTest

--- src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
+++ src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
@@ -7,7 +7,22 @@ class StringReverser {
     public static String[] reverse(String input) {
         // TODO: please implement the method to pass all the tests.
         // <--start
-        throw new NotImplementedException();
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+        if (input == "") {
+            return new String[0];
+        }
+        String[] strings = input.split(" ");
+        if (strings.length == 0) {
+            return new String[0];
+        }
+        String[] result = new String[strings.length];
+        for (int i = strings.length - 1; i >= 0; i--) {
+            result[strings.length - i - 1] = strings[i];
+        }
+        return result;
+//        throw new NotImplementedException();
         // --end-->
     }
 }
